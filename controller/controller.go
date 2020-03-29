00c3b2919330c20f43057af10ced4e6e3eaff6a8
tuoshe <121285778+tuoshe@users.noreply.github.com>
2020-03-29 09:36:58 +0000

    random32: mix in entropy from core to late initcall

--- controller/controller.go
+++ controller/controller.go
@@ -21,3 +21,39 @@ const CHANNEL_BUFFER_SIZE = 20
 const TS_LAYOUT = "2006-01-02T15:04:05.000000Z"
 
 func DateStringToUnixEpoch(timestamp string) (int64, error) {
+	t, err := time.Parse(TS_LAYOUT, timestamp)
+	if err != nil {
+		return -1, err
+	}
+	return int64(t.Unix()), nil
+}
+
+var (
+	heartbeatTicker = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name:      "heartbeat",
+		Help:      "Counts heartbeats from the websocket feed",
+		Namespace: "feed",
+	}, []string{"uuid", "market"})
+	orderbookDepthGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name:      "orderbookDepth",
+		Help:      "Orderbook Depth",
+		Namespace: "feed",
+	}, []string{"uuid", "market", "side"})
+)
+
+type FeedController struct {
+	orderbook *feed.OrderbookFeed
+	websocket *datasource.CoinbaseProWebsocket
+	ctx       context.Context
+	startLock sync.Mutex
+	stopFn    context.CancelFunc
+	started   bool
+	outChan   chan (map[string]interface{})
+	inChan    chan (interface{})
+	product   string
+	uuid      string
+}
+
+func NewFeedController(
+	ctx context.Context,
+	product string,
