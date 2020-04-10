8037ee97a40754ea23f4da9ff42df9deb79eba76
tuoshe <121285778+tuoshe@users.noreply.github.com>
2020-04-10 22:11:32 +0000

    Removed debugging code

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -6,4 +6,5 @@ func TestDateParsingWorks(t *testing.T) {
 	dateString := "2020-10-11T20:50:02.941691Z"
 	expectedResult := int64(1602449402)
 	result, _ := DateStringToUnixEpoch(dateString)
-	if result !
+	if result != expectedResult {
+		
