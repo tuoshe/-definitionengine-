6babc38a1f2b4861476c74115c5dbea3502fdc2a
tuoshe <121285778+tuoshe@users.noreply.github.com>
2020-05-13 20:04:30 +0000

    test connection

--- controller/service_client.go
+++ controller/service_client.go
@@ -40,4 +40,4 @@ func (ob *OrderbookGrpcController) handleResponse(response float64, lastUpdated 
 	}, nil
 }
 
-func (ob OrderbookGrpcController) BuyBase(ctx context.Co
+func (ob OrderbookGrpcController) BuyBase(ctx context.Context, in *rpc.Pric
