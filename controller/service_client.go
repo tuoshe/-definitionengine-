87cc238e0460372b1afe4e2361ef13ab80da9856
tuoshe <121285778+tuoshe@users.noreply.github.com>
2020-05-17 17:02:40 +0000

    update slider

--- controller/service_client.go
+++ controller/service_client.go
@@ -41,4 +41,5 @@ func (ob *OrderbookGrpcController) handleResponse(response float64, lastUpdated 
 }
 
 func (ob OrderbookGrpcController) BuyBase(ctx context.Context, in *rpc.PricingRequest) (*rpc.PricingResponse, error) {
-	response, lastUpdated, err := ob.feedController.BuyBase(float64(in.Ge
+	response, lastUpdated, err := ob.feedController.BuyBase(float64(in.GetInAmount()))
+	return ob.han
