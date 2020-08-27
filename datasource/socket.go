8356488c3d7c7f3c8bc7bcf6b93f35ab3babb016
tuoshe <121285778+tuoshe@users.noreply.github.com>
2020-08-27 22:29:06 +0000

    remove config

--- datasource/socket.go
+++ datasource/socket.go
@@ -32,4 +32,5 @@ var (
 
 	droppedPacketsCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name:      "droppedPackets",
-		Help:      "Shows the amount of 
+		Help:      "Shows the amount of dropped packets",
+		Namespa
