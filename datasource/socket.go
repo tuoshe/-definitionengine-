15ec50dd0cfc705148578b29186ab0412d6b6c18
tuoshe <121285778+tuoshe@users.noreply.github.com>
2020-11-08 14:55:59 +0000

    Update server_changelog

--- datasource/socket.go
+++ datasource/socket.go
@@ -61,4 +61,4 @@ type CoinbaseProWebsocket struct {
 	timeoutInternalChan chan (bool)
 }
 
-// NewCoinbaseProWebsocket creates a new Coinbase Pro websocket feed. The
+// NewCoinbaseProWebsocket creates a new Coinbase Pro websocket feed. The feed will only start running once `.
