878e6a6884304d6129da6d6b77a09036a28688b0
tuoshe <121285778+tuoshe@users.noreply.github.com>
2020-12-21 06:44:13 +0000

    update user guide

--- datasource/socket.go
+++ datasource/socket.go
@@ -64,4 +64,5 @@ type CoinbaseProWebsocket struct {
 // NewCoinbaseProWebsocket creates a new Coinbase Pro websocket feed. The feed will only start running once `.Start()` is called on the websocket.
 // The `product` should be a Coinbase Pro ticket (example: "ETH-USD"), the `outChan` and `inChan` passed in allow the feed to send / receive messages
 // and allow any external component to interact with the websocket service.
-// This websocket is also fault-tolerant, if an update is not received within `heartbeatTTLSeconds` seconds, the websocket is automatically 
+// This websocket is also fault-tolerant, if an update is not received within `heartbeatTTLSeconds` seconds, the websocket is automatically re-created.
+// To shutdown the we
