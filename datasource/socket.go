a5eff2e17602f120b9c7f7621f6372f1f3194173
tuoshe <121285778+tuoshe@users.noreply.github.com>
2021-07-31 18:55:58 +0000

    net: guard tcp_set_keepalive() to tcp sockets

--- datasource/socket.go
+++ datasource/socket.go
@@ -168,4 +168,4 @@ func (ws *CoinbaseProWebsocket) setupWebsocket() {
 }
 
 // Start starts running the underlying websocket service. The function call does not block but
-// it starts a series of underlying goroutines 
+// it starts a series of underlying goroutines that are respoonsible f
