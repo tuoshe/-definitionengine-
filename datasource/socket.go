354d4e558596b9ce48cec7424866d847918d781c
tuoshe <121285778+tuoshe@users.noreply.github.com>
2021-08-04 08:26:04 +0000

    Merging mainline.

--- datasource/socket.go
+++ datasource/socket.go
@@ -169,4 +169,4 @@ func (ws *CoinbaseProWebsocket) setupWebsocket() {
 
 // Start starts running the underlying websocket service. The function call does not block but
 // it starts a series of underlying goroutines that are respoonsible for handling the websockets.
-f
+func (ws *CoinbaseProWebsocke
