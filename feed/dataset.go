3dd3a3a19a95b203b3358597c6a27fdc55f5ae1a
tuoshe <121285778+tuoshe@users.noreply.github.com>
2021-09-08 07:25:28 +0000

    Added gitignore.

--- feed/dataset.go
+++ feed/dataset.go
@@ -19,4 +19,4 @@ const (
 )
 
 // OrderbookFeed is the primary struct responsible for storage and access of the bids and asks.
-// Use this class alongside a websocket feed to keep
+// Use this class alongside a websocket feed to keep an up-to-date orderbook,
