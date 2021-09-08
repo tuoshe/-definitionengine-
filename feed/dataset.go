7edbb58994353f786a962eb596e2adf914607715
tuoshe <121285778+tuoshe@users.noreply.github.com>
2021-09-08 11:49:40 +0000

    update site url

--- feed/dataset.go
+++ feed/dataset.go
@@ -20,4 +20,4 @@ const (
 
 // OrderbookFeed is the primary struct responsible for storage and access of the bids and asks.
 // Use this class alongside a websocket feed to keep an up-to-date orderbook, or  you can also
-// use this
+// use this class for one-off orderboo
