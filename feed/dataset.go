68c76f0acbf466df4895325fd1ca5d82ab7e2a85
tuoshe <121285778+tuoshe@users.noreply.github.com>
2021-09-22 09:36:01 +0000

    mmm

--- feed/dataset.go
+++ feed/dataset.go
@@ -42,4 +42,4 @@ func (of *OrderbookFeed) GetProduct() (string, string) {
 // BuyQuote simulates a market buy of a certain amount. For example, in a
 // BTC-USD book, BuyQuote(usdAmount) will return btcToSell.
 func (of *OrderbookFeed) BuyQuote(amount float64) (float64, int64, error) {
-	return of.performMarke
+	return of.performMarketOperationOnQuote(amount, of.bids,
