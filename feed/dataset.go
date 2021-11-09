f4a747a169cd1a948ec57040288e31f99223707b
tuoshe <121285778+tuoshe@users.noreply.github.com>
2021-11-09 20:43:49 +0000

    remove deprecation warnings

--- feed/dataset.go
+++ feed/dataset.go
@@ -53,4 +53,5 @@ func (of *OrderbookFeed) SellQuote(amount float64) (float64, int64, error) {
 
 // CleanUpOrderbook performs housekeeping on the books, by merging and removing
 // orders that have no size.
-func (of *OrderbookFeed) CleanUpOrder
+func (of *OrderbookFeed) CleanUpOrderbook() {
+	of.updateLock.Lock(
