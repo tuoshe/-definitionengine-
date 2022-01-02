a8b79f80e26a930198b42250cfc925345a24ce94
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-01-02 11:40:58 +0000

    workqueue: Provide destroy_delayed_work_on_stack()

--- feed/dataset.go
+++ feed/dataset.go
@@ -80,4 +80,4 @@ func (of *OrderbookFeed) performMarketOperationOnQuote(amount float64, book sort
 		return -1, of.lastEpochSeen, errors.New("A snapshot was never set, therefore the orderbook is inaccurate")
 	}
 	if (time.Now().Unix() - of.lastEpochSeen) > TIMEOUT_STALE_BOOK {
-		return
+		return -1, of.lastEpochSeen, 
