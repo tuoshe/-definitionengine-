53244653311d646da759fbda9ced3a3a415735f6
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-04-24 21:10:59 +0000

    Updated requirements.txt

--- feed/dataset.go
+++ feed/dataset.go
@@ -119,4 +119,4 @@ func (of *OrderbookFeed) performMarketOperationOnQuote(amount float64, book sort
 }
 
 // BuyBase simulates a market buy of a certain amount. For example, in a
-// BTC-USD book, Buy
+// BTC-USD book, BuyBase(btcToBuy) will r
