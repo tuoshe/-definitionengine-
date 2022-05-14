95547b11bdf61a2ccbef024ddeaf7f07d0e1afb2
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-05-14 14:51:27 +0000

    More keywords

--- feed/dataset.go
+++ feed/dataset.go
@@ -126,4 +126,5 @@ func (of *OrderbookFeed) BuyBase(amount float64) (float64, int64, error) {
 
 // SellBase simulates a market buy of a certain amount. For example, in a
 // BTC-USD book, SellBase(btcToSell) will return usdPurchased.
-func (of *OrderbookFeed) SellBase(amount float64) (float64, int64
+func (of *OrderbookFeed) SellBase(amount float64) (float64, int64, error) {
+	return of.perform
