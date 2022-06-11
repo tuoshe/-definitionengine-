bb405ee296881999d5b94de361c4095cd6e7f2aa
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-06-11 21:59:22 +0000

    Slight optimization

--- feed/dataset.go
+++ feed/dataset.go
@@ -135,4 +135,6 @@ func (of *OrderbookFeed) performMarketOperationOnBase(amount float64, book sortB
 		return -1, of.lastEpochSeen, errors.New("A snapshot was never set, therefore the orderbook is inaccurate")
 	}
 	if (time.Now().Unix() - of.lastEpochSeen) > TIMEOUT_STALE_BOOK {
-		return -1, of.lastEpochSeen, errors.New("Orderbook is stale"
+		return -1, of.lastEpochSeen, errors.New("Orderbook is stale")
+	}
+	if amount <= 0 {
