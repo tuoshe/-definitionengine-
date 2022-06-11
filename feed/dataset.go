9ccea42a501e68e8b356f1ddad1691a146f3ac6e
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-06-11 10:54:13 +0000

    Added .gitignore files

--- feed/dataset.go
+++ feed/dataset.go
@@ -135,4 +135,4 @@ func (of *OrderbookFeed) performMarketOperationOnBase(amount float64, book sortB
 		return -1, of.lastEpochSeen, errors.New("A snapshot was never set, therefore the orderbook is inaccurate")
 	}
 	if (time.Now().Unix() - of.lastEpochSeen) > TIMEOUT_STALE_BOOK {
-		return -1, of.lastEpochSeen, e
+		return -1, of.lastEpochSeen, errors.New("Orderbook is stale"
