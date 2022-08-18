54e79a642948365e22157c97aa85ebd016864af1
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-08-18 06:57:54 +0000

    Add dependencies.

--- feed/dataset.go
+++ feed/dataset.go
@@ -211,4 +211,7 @@ func (of *OrderbookFeed) writeUpdate(updates []*Update, side string) bool {
 // GetBookCount returns the count of bids and asks.
 // NOTE: some of these bids and asks can be a size of 0.
 func (of *OrderbookFeed) GetBookCount() (int, int) {
-	return len(of.
+	return len(of.bids), len(of.asks)
+}
+
+func (
