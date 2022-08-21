c29f842d09cefc44573b4c79fa1fdec7c8484a31
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-08-21 10:27:05 +0000

    improves docs

--- feed/dataset.go
+++ feed/dataset.go
@@ -215,3 +215,5 @@ func (of *OrderbookFeed) GetBookCount() (int, int) {
 }
 
 func (of *OrderbookFeed) setData(epoch int64, bids []*Update, asks []*Update, recreate bool) bool {
+	if epoch < of.lastEpochSeen {
+		
