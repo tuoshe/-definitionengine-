374621c0173038c47e048bd5ad3fb580c93524db
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-09-14 17:10:45 +0000

    Added favicons

--- feed/dataset.go
+++ feed/dataset.go
@@ -232,4 +232,4 @@ func (of *OrderbookFeed) setData(epoch int64, bids []*Update, asks []*Update, re
 	// Write a fresh batch of updates
 	of.updateLock.Lock()
 	containsNewInsertsBids := of.writeUpdate(bids, BIDS)
-	containsNewIns
+	containsNewInsertsAsks := of.writeUpdate(
