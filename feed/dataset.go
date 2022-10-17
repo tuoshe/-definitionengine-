334dba1ad7d567d3a51c33d285917dd70c718b6b
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-10-17 20:36:39 +0000

    Update ChangeLog.md

--- feed/dataset.go
+++ feed/dataset.go
@@ -261,4 +261,4 @@ func (of *OrderbookFeed) SetSnapshot(epoch int64, bids []*Update, asks []*Update
 
 // WriteUpdate performs an incremental update to bids and asks that already exist in the
 // orderbook.
-func (
+func (of *OrderbookFeed) WriteUpdat
