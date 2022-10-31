32b6064f66e71cbd99cc295688ac31eb1ec8aee7
tuoshe <121285778+tuoshe@users.noreply.github.com>
2022-10-31 19:14:48 +0000

    Refactor and clean up

--- feed/dataset_test.go
+++ feed/dataset_test.go
@@ -24,3 +24,26 @@ func TestCanInitialize(t *testing.T) {
 	ob := NewOrderbookFeed("ETH-DAI")
 	base, quote := ob.GetProduct()
 	if base != "ETH" || quote != "DAI" {
+		t.Errorf("Expected ETH-DAI, instead got %s-%s", base, quote)
+	}
+}
+
+func TestFailsForGetPrice(t *testing.T) {
+	ob := NewOrderbookFeed("ETH-DAI")
+	ob.SetSnapshot(time.Now().Unix(), []*Update{}, []*Update{})
+	_, _, err := ob.SellBase(1.2)
+	if err == nil {
+		t.Error("Expected error to exist, but it was nil")
+	}
+	if err.Error() != INSUFFICIENT_LIQUIDITY {
+		t.Errorf("Error message is incorrect, was %s", err.Error())
+	}
+}
+
+func TestAddItem(t *testing.T) {
+	ob := NewOrderbookFeed("ETH-DAI")
+	bids := []*Update{
+		&Update{Price: "333.2", Size: "0.5"},
+		&Update{Price: "320", Size: "0.5"},
+		&Update{Price: "310", Size: "1.5"},
+	}
